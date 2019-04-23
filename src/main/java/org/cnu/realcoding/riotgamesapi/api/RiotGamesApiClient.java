6d479d11c18d7574bdf6f9e8836265bd6d081db5
soomin <skeajd236@gmail.com>
2019-04-23 16:49:51 +0900

    [RAT-1] Allocated data named requestLeaguePositionDTO is modified.

--- src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
@@ -14,7 +14,7 @@ public class RiotGamesApiClient {
     private RestTemplate restTemplate;
     private static String APIKEY = "RGAPI-acc0822f-5c0f-4870-9184-f2f3a484f832";
     private String requestSummonerDTO = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={APIKEY}";
-    private String requestLeaguePositionDTO = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/{summonerName}?api_key={APIKEY}";
+    private String requestLeaguePositionDTO = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/{encryptedSummonerId}?api_key={APIKEY}";
 
     public SummonerDTO getCurrentSummonerDTO(String summonerName) {
         return restTemplate.exchange(requestSummonerDTO, HttpMethod.GET, null, SummonerDTO.class, summonerName).getBody();
