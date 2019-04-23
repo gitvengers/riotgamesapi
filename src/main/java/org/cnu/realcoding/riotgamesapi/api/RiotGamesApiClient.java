82395f59e874d26958d61a72ff617b7c4e62b599
soomin <skeajd236@gmail.com>
2019-04-23 16:06:40 +0900

    [RAT-1] Add annotation named Autowired to restTemplate in RiotGamesApiClient.

--- src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
@@ -1,11 +1,13 @@
 package org.cnu.realcoding.riotgamesapi.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RiotGamesApiClient {
 
+    @Autowired
     private RestTemplate restTemplate;
     private static String APIKEY;
     private String requestSummonerDTO;
