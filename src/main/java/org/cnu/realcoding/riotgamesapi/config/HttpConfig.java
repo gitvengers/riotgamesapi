6664380d92c9a248cbd89e306fb4d5627418c69d
soomin <skeajd236@gmail.com>
2019-04-23 16:04:30 +0900

    [RAT-4] Add annotation named Bean to createRestTemplate method.

--- src/main/java/org/cnu/realcoding/riotgamesapi/config/HttpConfig.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/config/HttpConfig.java
@@ -1,11 +1,13 @@
 package org.cnu.realcoding.riotgamesapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpConfig {
-    
+
+    @Bean
     public RestTemplate createRestTemplte() {
         return new RestTemplate();
     }
