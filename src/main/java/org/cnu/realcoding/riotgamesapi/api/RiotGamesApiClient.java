8065364d86bef8f89919a907b42729c0805e5bfc
soomin <skeajd236@gmail.com>
2019-04-23 14:53:29 +0900

    Add annotation named Service to RiotGamesApiClient.

--- src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
@@ -1,10 +1,14 @@
 package org.cnu.realcoding.riotgamesapi.api;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class RiotGamesApiClient {
+    
     private RestTemplate restTemplate;
     private static String APIKEY;
     private String requestSummonerDTO;
     private String requestLeaguePositionDTO;
+
 }
