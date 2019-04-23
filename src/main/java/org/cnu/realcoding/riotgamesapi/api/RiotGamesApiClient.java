cdf1a00b218e5522f98576d68f4261a370dcb964
soomin <skeajd236@gmail.com>
2019-04-23 16:32:04 +0900

    [RAT-1] Allocate APIKEY and url to the field values.

--- src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
@@ -11,9 +11,9 @@ public class RiotGamesApiClient {
 
     @Autowired
     private RestTemplate restTemplate;
-    private static String APIKEY;
-    private String requestSummonerDTO;
-    private String requestLeaguePositionDTO;
+    private static String APIKEY = "RGAPI-acc0822f-5c0f-4870-9184-f2f3a484f832";
+    private String requestSummonerDTO = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={APIKEY}";
+    private String requestLeaguePositionDTO = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/{summonerName}?api_key={APIKEY}";
 
     public SummonerDTO getCurrentSummonerDTO(String summonerName) {
         return restTemplate.exchange(requestSummonerDTO, HttpMethod.GET, null, SummonerDTO.class, summonerName).getBody();
