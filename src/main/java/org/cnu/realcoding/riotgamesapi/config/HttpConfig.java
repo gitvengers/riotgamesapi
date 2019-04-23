c93c43a289659591efee0b8534dfc822194a0de5
soomin <skeajd236@gmail.com>
2019-04-23 15:52:44 +0900

    [RAT-4] Add createRestTemlate method in HttpConfig class.

--- src/main/java/org/cnu/realcoding/riotgamesapi/config/HttpConfig.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/config/HttpConfig.java
@@ -1,8 +1,12 @@
 package org.cnu.realcoding.riotgamesapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpConfig {
-
+    
+    public RestTemplate createRestTemplte() {
+        return new RestTemplate();
+    }
 }
