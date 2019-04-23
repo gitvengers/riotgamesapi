e1c88c1c142f0f48af7c649fc28b6184f0c4d925
soomin <skeajd236@gmail.com>
2019-04-23 16:24:12 +0900

    [RAT-2] Add getCurrentSummonerDTO method in RiotGamesApiClient

--- src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/api/RiotGamesApiClient.java
@@ -1,6 +1,8 @@
 package org.cnu.realcoding.riotgamesapi.api;
 
+import org.cnu.realcoding.riotgamesapi.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,5 +15,7 @@ public class RiotGamesApiClient {
     private String requestSummonerDTO;
     private String requestLeaguePositionDTO;
 
-
+    public SummonerDTO getCurrentSummonerDTO(String summonerName) {
+        return restTemplate.exchange(requestSummonerDTO, HttpMethod.GET, null, SummonerDTO.class, summonerName).getBody();
+    }
 }
